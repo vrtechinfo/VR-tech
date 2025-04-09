3d606783493e5c9f55c37a3b2d1f37f0e0afede9
AKSHAYRAOCH <chakshayrao@gmail.com>
2025-04-10 00:01:30 +0530

    Add landing page with hero, features, services, and contact form sections

--- src/app/(rootpages)/page.tsx
+++ src/app/(rootpages)/page.tsx
@@ -9,27 +9,9 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
 import WhyChooseUs from "@/components/WhyChoose";
-import { useEffect, useRef } from "react";
-import { useSearchParams } from "next/navigation";
 import type { JSX } from "react";
 
-
-
-
 export default function Home(): JSX.Element {
-	const searchParams = useSearchParams();
-	const contactFormRef = useRef<HTMLDivElement>(null);
-	
-	useEffect(() => {
-		// Check if the URL has the contact=true parameter
-		if (searchParams.get('contact') === 'true') {
-			// Scroll to the contact form with a slight delay to ensure the page is fully loaded
-			setTimeout(() => {
-				contactFormRef.current?.scrollIntoView({ behavior: 'smooth' });
-			}, 500);
-		}
-	}, [searchParams]);
-	
 	return (
 		<div className="h-full w-full">
 			<Hero />
@@ -39,7 +21,7 @@ export default function Home(): JSX.Element {
 				<WhyChooseUs />
 				<Testimonials />
 				<div 
-					ref={contactFormRef}
+					id="contact"
 					className="absolute left-0 sm:left-[5%] md:left-[10%] lg:left-[10%] top-[600px] sm:top-[500px] md:top-[600px] lg:top-[1100px] w-full sm:w-11/12 md:w-10/12 lg:w-3/4 z-10"
 				>
 					<ContactForm />
